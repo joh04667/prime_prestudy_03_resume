e25511db1d1b1c1d89af77cb0406231fd31765e8
joh04667 <joh04667@gmail.com>
2016-03-20 23:15:12 -0500

    Finished
    All done!

---
+++ application.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#button > h1').on('click', function() {
+  	$(this).fadeOut();
+  	$('.instaFade').css({'display': 'block'});
+  	$('#button').hide();
+  });
+ss
+
+
+
+
+
+
+
+
+})
